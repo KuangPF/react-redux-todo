bc1223d120618fce6cd7cc5669a384a937f088ab
KuangPF <1633397286@qq.com>
2019-04-01 17:47:45 +0800

    feat: init AddTodo component

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import AddTdo from './components/AddTodo'
 
 export default function TodoApp() {
   return (
     <div>
       <h1>Todo List</h1>
+      <AddTdo />
     </div>
   )
 }
